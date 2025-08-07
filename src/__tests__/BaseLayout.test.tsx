a6d600d4bad44577bee0ab2ac153eb4014aa35dd
v.svir <vladsvir888@gmail.com>
2025-08-07 21:22:55 +0300

    fix: for mentor

--- src/__tests__/BaseLayout.test.tsx
+++ src/__tests__/BaseLayout.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import BaseLayout from '../layouts/Base';
 import { BrowserRouter } from 'react-router';
@@ -7,6 +7,10 @@ import ReduxProvider from '../providers/redux';
 
 describe('BaseLayout', () => {
   it('renders base layout with header', () => {
+    globalThis.matchMedia = vi.fn().mockReturnValue({
+      matches: false,
+    });
+
     render(
       <BrowserRouter>
         <ReduxProvider>
