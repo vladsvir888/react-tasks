2af9de52d43f18fcac0220ae477233ce48d67101
v.svir <vladsvir888@gmail.com>
2025-08-17 01:32:55 +0300

    chore: test deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  distDir: './dist',
   images: {
     remotePatterns: [
       {
